267bae52e8e87add9293189547a4a259d805c3ff
unknown <memorytemp5@gmail.com>
2024-03-19 20:53:49 +0530

    change server 1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 app.use(cors({
-    // origin: 'http://localhost:3000',
+    origin: 'https://login-backend-dph4.onrender.com',
     credentials: true, // Allow credentials (cookies)
 }));
 
